8414c5ecc6913579825589a3657126635ed48230
Menelao6 <menelao.pone@gmail.com>
2025-04-04 15:25:34 +0200

    section done

--- almer/app/components/Services/ServicesPage.js
+++ almer/app/components/Services/ServicesPage.js
@@ -3,14 +3,17 @@ import HeroSection from './HeroSection/HeroSection.js'
 import Styles from "./HeroSection/HeroSection.module.css"
 import Navbar from '../Navbar/Navbar.js'
 import Footer from '../HomePage/Footer/Footer.js'
+import CustomizationJourney from './CustomizationJourney/CustomizationJourney.js'
+import ServiceSpecializations from './ServiceSpecialization/ServiceSpecializations.js'
 
 export default function ServicesPage() {
   return (
     <div className={Styles.servicesPage}>
       <Navbar />
       <HeroSection />
+      <CustomizationJourney />
+      <ServiceSpecializations />
       <Footer />
-      {/* Add other sections */}
     </div>
   )
 }
