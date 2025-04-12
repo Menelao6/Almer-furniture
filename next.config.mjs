a494435e402b6b919f9dfbdc3cdc1ab90c3e39fe
Menelao6 <menelao.pone@gmail.com>
2025-04-12 15:24:11 +0200

    q

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/Almer-furniture/' : '',
-  basePath: isProd ? '/Almer-furniture' : '',
+  
 };
 
 export default nextConfig;
